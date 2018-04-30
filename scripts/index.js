6aaccfb14a84f0c5724eb3090be865bdee784f1e
Sean <38589300+CoolCoolSweet@users.noreply.github.com>
2018-04-29 22:45:41 -0400

    some test button up, everything linked.

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,16 @@
-console.log("hi");
+// $(function () {
+    
+    // VARIABLES
+    const playButtons = document.getElementsByClassName('play-button');
+
+    // FUNCTIONS
+    const clickEvent = () => {
+        console.log("hi");
+    };
+
+
+    for (let i=0; i < playButtons.length; i++) {
+    playButtons[i].addEventListener("click", clickEvent, false);
+    };
+
+// });
